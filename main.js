9a1f1efcf86b657b193922b43b2b5ed3d9c0602b
fuchsschneckenwild <pokefoxone@gmail.com>
2022-12-01 23:25:22 +0100

    improved logic, added music

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const calendarContainer = document.querySelector(".container");
-const audio = document.querySelector("audio");
+const audioJukebox = document.getElementById("jukebox");
+const audioXmas = document.getElementById("xmas");
 
 const currentDate = new Date();
 const currentDay = currentDate.getDate();
@@ -19,19 +20,23 @@ const openDoorSecond = (day, event) => {
 
 const openDoorThird = (day, event) => {
     event.target.style.opacity = "0";
-    if (day != 24) {
-        event.target.addEventListener("click", openDoorFourth.bind(null, day), { once: true });
-    }
     sumOfDays = sumOfDays + day;
-    if (sumOfDays == 300) { // Summe von 1 bis 24
-        calendarContainer.style.backgroundImage = `url(./images/calendar24.png)`;
+    if (day != 24 && sumOfDays != 300) { // Summe von 1 bis 24
+        event.target.addEventListener("click", openDoorFourth.bind(null, day), { once: true });
+    } else if (sumOfDays == 300) {
+        audioJukebox.pause();
         document.querySelectorAll(".image").forEach((e) => e.parentNode.removeChild(e));
-        audio.play();
+        calendarContainer.style.backgroundImage = `url(./images/calendar24.png)`;
+        audioXmas.play();
     }
 }
 
 const openDoorFourth = (day) => {
-    window.open("./tueren/tuer" + day + ".html");
+    if (day != 6) {
+        window.open("./tueren/tuer" + day + ".html");
+    } else {
+        audioJukebox.play();
+    }
 }
 
 const createCalendar = () => {
